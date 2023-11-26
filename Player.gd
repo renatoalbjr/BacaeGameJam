3ef106d8a8bc04298b8b43a1c2fbe147efc606ca
luancarvalho <luancarvalho@pactosolucoes.com.br>
2023-11-26 14:59:55 -0300

    Fix Jump Animation

--- Player.gd
+++ Player.gd
@@ -3,61 +3,72 @@ extends CharacterBody2D
 @export var SPEED = 300
 @export var JUMP_VELOCITY = 500
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Direção do movimento do personagem.
+@export var direction = 0.0
+
+# Variável que indica se o personagem está pulando no momento.
+var is_jumping: bool = false
+
+# Obtendo a gravidade do ambiente do jogo.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Referência ao nó do sprite animado do personagem.
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
-#	if not is_on_floor():
-#		velocity.y += gravity * delta
-#		sprite.play("on_air")
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		sprite.play("jump_squat")
-		if sprite.is_playing() and sprite.animation == "jump_squat":
-			return
-		velocity.y = -JUMP_VELOCITY
+	# Aplicação da gravidade quando o personagem não está no chão.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 		
+	# Obtendo a direção do movimento baseado na entrada do jogador.
+	direction = Input.get_axis("ui_left", "ui_right")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# Chamada da função para lidar com a mudança de direção.
+	handle_direction_change()
+
+	# Lidando com as animações para movimento e saltos.
+	if is_on_floor() and sprite.animation != "jump_squat":
+		if direction == 0.0:
+			sprite.play("idle")
+		else:
+			sprite.play("run")
+
+	# Chamada da função para lidar com eventos de salto.
+	handle_jump_event()
+		
+	# Lidando com a ação do inventário quando o botão de baixo é pressionado.
+	if Input.is_action_just_pressed("ui_down"):
+		if $Inventory.has_node("Item1"):
+			$Inventory/Item1.use_item()
+			
+	# Movimentando o personagem com tratamento de colisão.
+	move_and_slide()
+
+func handle_direction_change():
+	# Movendo o personagem na direção determinada ou desacelerando-o se nenhuma direção for pressionada.
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if is_on_floor():
-		if direction == 0.0:
-			sprite.play("idle")
-		else:
-			sprite.play("run")
-		
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		sprite.play("on_air")
-		
+	# Invertendo o sprite com base na direção do movimento.
 	if direction > 0.0:
 		sprite.flip_h = false
 	elif direction < 0.0:
 		sprite.flip_h = true
 
-	if Input.is_action_just_pressed("ui_down"):
-		if $Inventory.has_node("Item1"):
-			$Inventory/Item1.use_item()
-			
+func handle_jump_event():
+	# Executando o salto se o personagem estiver na fase de "jump_squat".
+	if !sprite.is_playing() and sprite.animation == "jump_squat":
+		velocity.y = -JUMP_VELOCITY
+		sprite.play("on_air")
 
-	move_and_slide()
-
-
-
-
-
+	# Ativando o salto quando o botão de salto é pressionado e o personagem está no chão.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		is_jumping = true
+		sprite.play("jump_squat")
+		
+# Lidando com eventos de colisão.
 func _on_hitbox_body_entered(body):
 	if body.is_in_group("Enemy"):
 		queue_free()
-
-
